f227a90f62c64c8293863010767c1c8cfe763ecc
Farouk ECH-CHAREF <fech-cha@localhost.localdomain>
2022-06-08 10:54:42 +0100

    Bash Scripting Training

--- 5the_world_of_number.sh
+++ 5the_world_of_number.sh
@@ -8,8 +8,48 @@
 #    NoOfInputs--
 #done
 
+#My solution
 #-a helps script to read an array
 
-read -a INTS
-ADD=$INTS[0]+$INTS[1]
-echo $ADD
+#read -a INTS
+#let "ADD=${INTS[0]}+${INTS[1]}"
+#let "SUB=${INTS[0]}-${INTS[1]}"
+#let "MUL=${INTS[0]}*${INTS[1]}"
+#let "DIV=${INTS[0]}/${INTS[1]}"
+#echo -e "$ADD\n"
+#echo -e "$SUB\n"
+#echo -e "$MUL\n"
+#echo -e "$DIV\n"
+
+#HackerRank Custom Solution
+
+
+#read FIRST 
+#read SECOND
+#$((...)) means perform arithmetic 
+#and return the result of the calculation
+#let "ADD=$((FIRST+SECOND))"
+#let "SUB=$((FIRST-SECOND))"
+#let "MUL=$((FIRST*SECOND))"
+#let "DIV=$((FIRST/SECOND))"
+
+#echo ${ADD}
+#echo ${SUB}
+#echo ${MUL}
+#echo ${DIV}
+
+#The Best Approach
+
+read FIRST
+read SECOND
+
+if [ $FIRST -le "100" ] && [ $SECOND -le "100" ] && [ $FIRST -ge "-100" ] && [ $SECOND -ge "-100" ] && [ $SECOND -ne "0" ]
+then
+    for operate in {"+","-","*","/",}
+    do
+        echo "$FIRST $operate $SECOND" | bc
+    done
+else
+    echo "Error of Input"
+fi
+
