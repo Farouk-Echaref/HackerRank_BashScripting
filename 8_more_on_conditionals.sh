0f1b860ad1ea6b1bc7502d2ba015f63480bd071d
Farouk ECH-CHAREF <fech-cha@localhost.localdomain>
2022-06-18 02:52:10 +0100

    learning bash

--- 8_more_on_conditionals.sh
+++ 8_more_on_conditionals.sh
@@ -1,6 +1,15 @@
-#1/bin/bash
+#!/bin/bash
 
-read X
-read Y 
-read Z 
+read x
+read y
+read z 
 
+if [[ $x -eq $y && $x -eq $z ]]
+then 
+    echo "EQUILATERAL"
+elif [[ $x -eq $y || $x -eq $z || $y -eq $z ]]
+then    
+    echo "ISOSCELES"
+else
+    echo "SCALENE"
+fi
